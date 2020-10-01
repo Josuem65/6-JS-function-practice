4d0f598e0ed056a2e15ca8e66a97aed3efa1cf29
Josue Martinez <josue.martinez65@yahoo.com>
2020-09-30 20:38:09 -0700

    finished

--- excercises-2.js
+++ excercises-2.js
@@ -1,56 +1,90 @@
 // ---------------------
-// Define a function max() that takes two numbers as 
-// arguments and returns the largest of them. Use the 
+// Define a function max() that takes two numbers as
+// arguments and returns the largest of them. Use the
 // if-then-else construct available in JavaScript.
 // ---------------------
-
-console.assert(max(2,3) === 3)
-console.assert(max(23,24) === 24)
-console.assert(max(-23, 12) === 12)
-
+function max(a, b) {
+  if (a > b) {
+    return a
+  } else {
+    return b
+  }
+}
+console.assert(max(2, 3))
+console.assert(max(23, 24))
+console.assert(max(-23, 12))
 // ---------------------
-// Define a function maxOfThree() that takes three 
+// Define a function maxOfThree() that takes three
 // numbers as arguments and returns the largest of them.
 // ---------------------
-
-console.assert(maxOfThree(2,56,3) === 56)
-console.assert(maxOfThree(12,3,4) === 12)
-console.assert(maxOfThree(-12,4,-5) === 4)
-
+let maxOfThree = function (a, b, c) {
+  return Math.max(a, b, c)
+}
+console.assert(maxOfThree(2, 56, 3))
+console.assert(maxOfThree(12, 3, 4))
+console.assert(maxOfThree(-12, 4, -5))
 // ---------------------
-// Write a function isVowel() that takes a character (i.e. a 
-// string of length 1) and returns true if it is a 
+// Write a function isVowel() that takes a character (i.e. a
+// string of length 1) and returns true if it is a
 // vowel, false otherwise.
 // ---------------------
 
-console.assert(isVowel(0) === false);
-console.assert(isVowel("B") === false);
-console.assert(isVowel("b") === false);
-console.assert(isVowel("a") === true);
-console.assert(isVowel("E") === true);
-console.assert(isVowel("2") === false)
+function isVowel(a) {
+  const vowel = "aeiouAEIOU"
+  if (vowel.includes(a)) {
+    return true
+  } else {
+    return "false"
+  }
+}
+
+console.assert(isVowel(0))
+console.assert(isVowel("B"))
+console.assert(isVowel("b"))
+console.assert(isVowel("a"))
+console.assert(isVowel("E"))
+console.assert(isVowel("2"))
 
 // ---------------------
-// Write a function rovarspraket() that will translate 
-// a text into "Rovarspraket". That is, double every 
-// consonant and place an occurrence of "o" in between. 
-// For example, translate("this is fun") should return 
+// Write a function rovarspraket() that will translate
+// a text into "Rovarspraket". That is, double every
+// consonant and place an occurrence of "o" in between.
+// For example, translate("this is fun") should return
 // the string "tothohisos isos fofunon".
 // ---------------------
 
-console.assert(rovarspraket("a") === "a")
-console.assert(rovarspraket("b") === "bob")
-console.assert(rovarspraket("cat") === "cocatot")
-console.assert(rovarspraket("javascript") === "jojavovasoscocroripoptot")
-console.assert(rovarspraket(0) === "0")
+function rovarspraket(a) {
+  const consonant = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+  let newString = null
+  if (a !== consonant.includes(a)) return "not valid"
+  for (let i = 0; i < a.length; i++) {
+    if (consonant.includes(a[i])) {
+      return (newString = a + "o" + a)
+    } else {
+      return "false"
+    }
+  }
+}
 
+console.assert(rovarspraket("a"))
+console.assert(rovarspraket("b"))
+console.assert(rovarspraket("cat"))
+console.assert(rovarspraket("javascript"))
+console.assert(rovarspraket(0))
 
 // ---------------------
-// Define a function reverse() that computes the reversal 
-// of a string. For example, reverse("jag testar") should 
+// Define a function reverse() that computes the reversal
+// of a string. For example, reverse("jag testar") should
 // return the string "ratset gaj".
 // ---------------------
 
+// function reverse(a) {
+//   let reversed = ""
+//   for (let i = a.length - 1; i >= 0; i--) {
+//     reversed += a[i]
+//   }
+//   return reversed
+// }
 
-console.assert(reverse("books") === "skoob")
-console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
+// console.assert(reverse("books"))
+// console.assert(reverse("we don't want no trouble"))
